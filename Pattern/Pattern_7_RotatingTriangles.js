c93bb229786373db9bad56b0dcce9f7710d7be1e
Digri <m.digristina@gmail.com>
2024-06-03 12:15:44 +0200

    refuso e fix rotating triangle con no loop

--- Pattern/Pattern_7_RotatingTriangles.js
+++ Pattern/Pattern_7_RotatingTriangles.js
@@ -1,15 +1,18 @@
 let x = 0;
 let y = 0;
-let spacing = 50; //spacing
+let spacing;
+let cols = 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(236);
+  spacing = width/cols
 }
 
 function draw() {
   fill(41);
-  noStroke();
+  strokeWeight(1);
+  stroke (41)
   translate(x + spacing / 2, y + spacing / 2); // trasla origine nell'altra cella
 
   // Ruota a caso tra 0,90,180,270
@@ -26,4 +29,10 @@ function draw() {
     x = 0;
     y += spacing;
   }
+  
+      if (y > height) { 
+    noLoop();
+    console.log("altezza raggiunta");
+  }
+  
 }
